1c88983c6d761829515f82ee15b0178fc582987a
Umut Can Arda <umutkonurinso@gmail.com>
2020-12-16 21:21:24 +0300

    Grandle update

--- app/src/main/java/com/flysquare/gizumkefet/MainActivity.java
+++ app/src/main/java/com/flysquare/gizumkefet/MainActivity.java
@@ -1,14 +1,81 @@
 package com.flysquare.gizumkefet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.MapView;
+import com.huawei.hms.maps.MapsInitializer;
+import com.huawei.hms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MapViewDemoActivity";
+    //HUAWEI map
+    private HuaweiMap hMap;
+
+    private MapView mMapView;
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapView = findViewById(R.id.mapView);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        // please replace "Your API key" with api_key field value in
+        // agconnect-services.json
+        MapsInitializer.setApiKey("CgB6e3x9Xlc7SD/tl3K4zdY4JWoCeZOU4g+C2fwx8GK2kYDjz1dG9qvV2p8cYS0phdcHJobn6x7S5DvBI9iRsS4T");
+        mMapView.onCreate(mapViewBundle);
+        //get map instance
+        mMapView.getMapAsync(this);
     }
+    
+    @Override
+    public void onMapReady(HuaweiMap huaweiMap) {
+        Log.d(TAG, "onMapReady: ");
+        hMap = huaweiMap;
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+
 }
